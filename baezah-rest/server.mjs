99b12aed073671047be3aeaf0e221d007645e5fa
SFhectorj <SFhectorj@icloud.com>
2025-03-11 21:54:08 -0700

    Server

--- baezah-rest/server.mjs
+++ baezah-rest/server.mjs
@@ -8,4 +8,32 @@ dotenv.config()
 // Initialize Express: get PORT and connectionstring
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_CONNECT_STRING = 
+const MONGODB_CONNECT_STRING = process.env.MONGODB_CONNECT_STRING;
+
+// Parse JSON requests
+app.use(express.json());
+
+mongoose.connect(MONGODB_CONNECT_STRING, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+// Check for errors
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Show success message
+db.once('open', () => {
+    console.log('Connectedto MongoDB');
+});
+
+app.use('/excercises', excerciseRoutes);
+
+// Start express server
+app.listen(PORT, () => {
+    console.log('Server running on port: ${PORT}');
+});
+
+
+
